88efcf61c7389722238e8f407c7e3cb774e549cc
안예찬 <yes490411@gmail.com>
2025-07-14 14:37:52 +0900

    refactor : sqlite_handler를 도메인별 파일로 분리하고 database 폴더로 이동

---
+++ backend/sqlite_db/database/sqlite_handler.py
@@ -0,0 +1,20 @@
+"""
+통합 SQLite 핸들러
+모든 도메인별 핸들러를 통합하여 제공합니다.
+"""
+from .base_handler import BaseHandler
+from .brain_handler import BrainHandler
+from .memo_handler import MemoHandler
+from .pdf_handler import PdfHandler
+from .textfile_handler import TextFileHandler
+from .chat_handler import ChatHandler
+from .search_handler import SearchHandler
+
+
+class SQLiteHandler(BaseHandler, BrainHandler, MemoHandler, PdfHandler, TextFileHandler, ChatHandler, SearchHandler):
+    """
+    통합 SQLite 핸들러 클래스
+    모든 도메인별 핸들러의 기능을 상속받아 제공합니다.
+    """
+    def __init__(self, db_path=None):
+        BaseHandler.__init__(self, db_path) 
