2c45943167828b71d25476ac4241a69454ca79b4
안예찬 <yes490411@gmail.com>
2025-08-09 22:46:55 +0900

    docs : Add JSDoc comments to React components for better code documentation

--- frontend/src/components/layout/AppFooter.jsx
+++ frontend/src/components/layout/AppFooter.jsx
@@ -1,3 +1,17 @@
+/**
+ * AppFooter 컴포넌트
+ * 
+ * 이 컴포넌트는 애플리케이션의 하단 푸터를 렌더링합니다.
+ * 주요 기능:
+ * - 개발팀 정보 표시
+ * - 연락처 정보 (이메일) 표시
+ * - 저작권 정보 표시
+ * 
+ * 표시되는 정보:
+ * - 개발팀: brainTrace 개발팀
+ * - 이메일: contact@braintrace.com
+ * - 저작권: Copyright brainTrace All rights reserved.
+ */
 // src/components/layout/AppFooter.jsx
 import React from 'react';
 import './AppFooter.css';
